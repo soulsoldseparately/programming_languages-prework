2d7ce188e8ad48400528f5580af97a08e0274645
Daniel Toma <danieltoma@ubalumni.com>
2019-01-08 02:04:24 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+   languages.each do |style, value|
+    value.each do |k, v|
+      v.each do |key, val|
+
+         if new_hash.has_key?(k)
+          new_hash[k][:style] << style
+        else
+          new_hash[k] = {}
+          new_hash[k][:type] = val
+          new_hash[k][:style] = [] << style
+        end
+      end
+    end
+  end
+  new_hash
 end
